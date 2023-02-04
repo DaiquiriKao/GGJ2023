f8bcb6d94afa202cbe70b508f9db1318fc79e601
Hugo <Hugoliu1113@gmail.com>
2023-02-04 20:21:13 +0800

    bagDrag Script add

--- Assets/Scripts/Bag/BagDrag.cs
+++ Assets/Scripts/Bag/BagDrag.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,11 +10,16 @@ using UnityEngine.UIElements;
 
 public class BagDrag : MonoBehaviour
 {
-    public float DragItemEndDistance = 10.0f;
-    public Transform[] BagItems;
+    public float DragItemEndDistance = 1.0f;
+    private GameObject[] items;
+    private Transform[] BagItems;
+    private Merge merge;
+    private Bag bag;
 
     void Start()
     {
+        merge = FindObjectOfType<Merge>();
+        bag = GetComponent<Bag>();
         BagItems = GetComponentsInChildren<Transform>().Where(x => x.gameObject.name.Length > 6 && x.gameObject.name.Substring(0, 7) == "BagItem").ToArray();
     }
 
@@ -27,18 +33,26 @@ public class BagDrag : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            PointerEventData pointer = new PointerEventData(EventSystem.current);
-            pointer.position = Input.mousePosition;
-
-            List<RaycastResult> raycastResults = new List<RaycastResult>();
-            EventSystem.current.RaycastAll(pointer, raycastResults);
-
             Vector2 rayPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            if (raycastResults.Count > 0 && raycastResults[0].gameObject.tag == "Item")
+            if (mouseClickObject != null)
             {
-                raycastResults[0].gameObject.transform.position = rayPos;
-                mouseClickObject = raycastResults[0].gameObject;
+                mouseClickObject.transform.position = rayPos;
+            }
+            else
+            {
+                PointerEventData pointer = new PointerEventData(EventSystem.current);
+                pointer.position = Input.mousePosition;
+
+                List<RaycastResult> raycastResults = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(pointer, raycastResults);
+
+                if (raycastResults.Count > 0 && raycastResults[0].gameObject.tag == "Item")
+                {
+                    raycastResults[0].gameObject.transform.position = rayPos;
+                    mouseClickObject = raycastResults[0].gameObject;
+                    //bag.AddWord("");
+                }
             }
         }
         else if (mouseClickObject != null)
@@ -49,11 +63,38 @@ public class BagDrag : MonoBehaviour
                 if (Vector2.Distance(bagItem.position, mouseClickObject.transform.position) < DragItemEndDistance)
                 {
                     mouseClickObject.transform.position = bagItem.position;
+                    //若物體能合併則合併並刪除物件
+                    mergeItem(mouseClickObject);
                     mouseClickObject = null;
                     break;
                 }
             }
         }
-
     }
+
+    private void mergeItem(GameObject mouseClickObject)
+    {
+        if (mouseClickObject == null)
+            return;
+
+        items = GameObject.FindGameObjectsWithTag("Item");
+        foreach (GameObject item in items)
+        {
+            if (item.name == mouseClickObject.name
+                || Vector2.Distance(item.transform.position, mouseClickObject.transform.position) > DragItemEndDistance)
+                continue;
+
+            TMP_Text tragText = item.GetComponent<TMP_Text>();
+            TMP_Text thisText = mouseClickObject.GetComponent<TMP_Text>();
+
+
+
+            //todo : 判斷可否合併
+            item.GetComponent<TMP_Text>().text = tragText.text + thisText.text;
+
+            Destroy(mouseClickObject);
+            return;
+        }
+    }
+
 }
