dabaff31b7670f83f835a2f304c6b26c5ef03477
Hugo <Hugoliu1113@gmail.com>
2023-02-05 13:42:56 +0800

    add Teleport Script

--- Assets/Scripts/Energybar/Hurt.cs
+++ Assets/Scripts/Energybar/Hurt.cs
@@ -8,18 +8,21 @@ public class Hurt : MonoBehaviour
     public bool HurtForce = true;
     public string DamgeObjectName = "DamgeObject";
     public float Damge = 1.0f;
+    public float Power = 2.0f;
 
     public EnergyBar EnergyBar;
 
+    private Rigidbody2D rigidbody2;
+
     void Start()
     {
-        
+        rigidbody2 = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -32,7 +35,8 @@ public class Hurt : MonoBehaviour
             if (HurtForce)
             {
                 //todo: 受傷之後動作
-
+                Debug.Log();
+                //rigidbody2.AddForce(new Vector2(gameObject.transform.forward.x * -1, gameObject.transform.forward.y + 0.5f) * Power);
             }
         }
     }
