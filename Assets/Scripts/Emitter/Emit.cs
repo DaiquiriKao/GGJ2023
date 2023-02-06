18cc1d73858296308161802874c458e8cd9526b4
Wujoe0415 <wujoe0415@gmail.com>
2023-02-06 12:18:06 +0800

    Emit

--- Assets/Scripts/Emitter/Emit.cs
+++ Assets/Scripts/Emitter/Emit.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 
 public class Emit : MonoBehaviour
 {
+    public Transform EmitPoint;
     public GameObject Fireball;
+    public float Force = 1f;
     private IEnumerator coroutine;
-    private WaitForSeconds delay = new WaitForSeconds(0.8f);
+    private WaitForSeconds delay = new WaitForSeconds(1f);
     private Animator m_animator;
+
     
     private void Awake()
     {
@@ -30,13 +33,17 @@ public class Emit : MonoBehaviour
     }
     private IEnumerator StartEmit()
     {
-        EmitBall();
-        yield return delay;
+        yield return new WaitForSeconds(0.2f);
+        while (true)
+        {
+            EmitBall();
+            yield return delay;
+        }
     }
     public void EmitBall()
     {
         //m_animator.SetTrigger("Fire");
-        GameObject temp = GameObject.Instantiate(Fireball, transform.position, Quaternion.identity, transform);
-        temp.GetComponent<Rigidbody2D>().AddForce(new Vector2(1f, 1f), ForceMode2D.Impulse);
+        GameObject temp = GameObject.Instantiate(Fireball, EmitPoint.position, Quaternion.Euler(0f, 180f, 0f), transform);
+        temp.GetComponent<Rigidbody2D>().AddForce(Force * new Vector2(1f, 0f), ForceMode2D.Impulse);
     }
 }
